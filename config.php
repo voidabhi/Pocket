53a2ded4fcb961887cabc7bf9a8b734406628222
voidabhi <void.aby@gmail.com>
2014-03-09 01:01:49 +0530

    done

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+        
+        $config['consumer_key']='24148-39cb209cef54ab22efd1fb67';
+        
+        $config['redirect_uri']='http://pocketapp.pagodabox.com/callback.php';
+        
+        $config['access_token']='';
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+    require_once 'lib/Unirest.php';
+    require_once 'config.php';
+    
+    Unirest::verifyPeer(false);
+    
+    $data = array(
+        'consumer_key' => $config['consumer_key'], 
+	'redirect_uri' => $config['redirect_uri']
+     );    
+    
+    $response = Unirest::post("https://getpocket.com/v3/oauth/request", 
+            array("Content-Type" => "application/json; charset=UTF-8",
+               "X-Accept" => "application/json"),
+      json_encode($data)
+    );
+    
+    $code = $response->body->code;
+    
+    print_r($code);
+    
+    header("Location: https://getpocket.com/auth/authorize?request_token=$code&redirect_uri=".$config['redirect_uri']."?request_token=$code");
+?>
