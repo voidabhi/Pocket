2604568fa9f2d3b6ecf7fc94758e964e5ae30eb3
voidabhi <void.aby@gmail.com>
2014-03-09 10:41:19 +0530

    k

--- api.php
+++ api.php
@@ -31,8 +31,7 @@
 	<body>
 	<div class="container">
       <div class="page-header">
-        <h1>Bootstrap grid examples</h1>
-        <p class="lead">Basic grid layouts to get you familiar with building within the Bootstrap grid system.</p>
+        <h1>List of pocket links</h1>
       </div>
 		<ul>
 		<?
