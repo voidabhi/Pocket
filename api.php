9c283f61c91d6935d43a7cf3150d0cfce65c84a8
voidabhi <void.aby@gmail.com>
2014-03-09 16:26:45 +0530

    Initial Commit

--- api.php
+++ api.php
@@ -1,12 +1,9 @@
 <?php
 	session_start();
-	/* read the docs!
-		by default, I'm just returning the 5 most recent
-		pocket items.
-		read more here: http://getpocket.com/developer/docs/v3/retrieve
-	 */
+
         $config = $_SESSION['config'];
-        $access_token = explode("=",$config['access_token']);
+        print_r($config);
+   /*     $access_token = explode("=",$config['access_token']);
 	$url = 'http://getpocket.com/v3/get?count=5';
 	$data = array(
 		'consumer_key' => $config['consumer_key'],
@@ -19,10 +16,10 @@
       json_encode($data)
     );
 
-        $pocket_links = $response->body->list;
+        $pocket_links = $response->body->list;*/
 ?>
 
-<html>
+<?/*<html>
 	<head>
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet"/>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
@@ -42,3 +39,5 @@
 		</div>
 	</body>
 </html>
+
+*/?>
