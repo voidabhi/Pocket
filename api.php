fc474104c115434634455cf08d1bb9d35591644d
voidabhi <void.aby@gmail.com>
2014-03-09 09:55:24 +0530

    k

--- api.php
+++ api.php
@@ -21,5 +21,5 @@
 	$context  = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
         print_r($result);*/
-        print_r($config['access_token']);
+        print_r($_SESSION);
 ?>
