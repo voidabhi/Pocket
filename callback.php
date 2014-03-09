f06f811012cb66af9829c4dd0914bd49eeaa363d
voidabhi <void.aby@gmail.com>
2014-03-09 15:28:23 +0530

    Initial Commit

--- callback.php
+++ callback.php
@@ -20,18 +20,12 @@
 
 	$access_token = explode('&',$result);
 	if($access_token[0]!=''){
-		echo "<h1>You've been authenticated succesfully!</h1>";
-		echo "You should write down the access_token and then add it to config.php<br>";
-		echo "Your access token: ". $access_token[0];
-		echo "<br>";
-		echo "add this to config.php";
-	} else{
-		echo "Something went wrong. :( ";
-	}
-
         $config['access_token']=$access_token[0];
         $_SESSION['config'] = $config;
         header("location:api.php");
+	} else{
+		echo "Something went wrong. :( ";
+	}
 
 
 ?>
