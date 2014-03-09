62c8fab3b4add6b080da0dfa6ac7f98153d74d05
voidabhi <void.aby@gmail.com>
2014-03-09 10:26:54 +0530

    k

--- api.php
+++ api.php
@@ -26,12 +26,3 @@
 			}
 
 ?>
-
-<html>
-	<head>
-		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet"/>
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-	</body>
-</html>
