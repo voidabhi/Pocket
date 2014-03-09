4004a89bc75fa3e373c2f08f3ddb0b06c5cb80fa
voidabhi <void.aby@gmail.com>
2014-03-09 10:01:31 +0530

    k

--- api.php
+++ api.php
@@ -1,26 +1,23 @@
 <?php
 	//require_once('config.php');
 	session_start();
+	require_once 'lib/Unirest.php';
 	/* read the docs!
 		by default, I'm just returning the 5 most recent
 		pocket items.
 		read more here: http://getpocket.com/developer/docs/v3/retrieve
 	 */
         $config = $_SESSION['config'];
-	/*$url = 'http://getpocket.com/v3/get?count=5';
+	$url = 'http://getpocket.com/v3/get?count=5';
 	$data = array(
 		'consumer_key' => $config['consumer_key'],
 		'access_token' => $config['access_token']
 	);
-	$options = array(
-		'http' => array(
-			'method'  => 'POST',
-                        'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-			'content' => http_build_query($config)
-		)
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-        print_r($result);*/
-        print_r($_SESSION);
+
+    $response = Unirest::post($url,
+            array("Content-Type" => "application/json; charset=UTF-8",
+               "X-Accept" => "application/json"),
+      json_encode($data)
+    );
+        print_r($response);
 ?>
