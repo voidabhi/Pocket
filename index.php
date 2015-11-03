0053a1e50bc94ab4da16164ca2d79e1a1fe3bda8
Abhijeet Mohan <void.aby@gmail.com>
2015-11-03 21:37:46 +0530

    updated comments

--- index.php
+++ index.php
@@ -4,18 +4,20 @@
     require_once 'config.php';
 
     Unirest::verifyPeer(false);
-
+    
+    // fetch consumer key and redirect uri from config file
     $data = array(
         'consumer_key' => $config['consumer_key'],
 	'redirect_uri' => $config['redirect_uri']
      );
-
+     
+     
+    // request for access token
     $response = Unirest::post("https://getpocket.com/v3/oauth/request",
             array("Content-Type" => "application/json; charset=UTF-8",
                "X-Accept" => "application/json"),
       json_encode($data)
     );
-
     $code = $response->body->code;
 
     // printing request code
