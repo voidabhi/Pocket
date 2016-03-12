90d2b9fd5a3eb3894b52a7791e9d15ca73d18eb2
Abhijeet Mohan <void.aby@gmail.com>
2016-03-12 23:46:27 +0530

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,1 +1,16 @@
 
+<?php
+
+define('APP_ROOT', __DIR__.'/../');
+ 
+require APP_ROOT.'/vendor/autoload.php';
+ 
+date_default_timezone_set('Asia/Kolkata');
+ 
+// Require all classes
+ 
+spl_autoload_register(function ($class_name) {
+ 
+  require_once APP_ROOT."/config.php";
+  require_once APP_ROOT."/api.php";
+});
