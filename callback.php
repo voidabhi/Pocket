1d6e844e2c8cea3d0fad544d6763c9fbc623415a
Abhijeet Mohan <void.aby@gmail.com>
2016-09-04 23:50:18 +0530

    fix styling

--- callback.php
+++ callback.php
@@ -1,37 +1,36 @@
 <?php
-    	session_start();
-	
-	// Fetch request token from get parameter
-	$request_token = $_GET['request_token'];
+session_start();
 
-	// pocket authorization url
-	$url = 'https://getpocket.com/v3/oauth/authorize';
-	$data = array(
-		'consumer_key' => $config['consumer_key'],
-		'code' => $request_token
-	);
+// Fetch request token from get parameter
+$request_token = $_GET['request_token'];
 
-	// making post request with the data
-	   $response = Unirest::post($url,
-	            array("Content-Type" => "application/json; charset=UTF-8",
-	               "X-Accept" => "application/json"),
-	      json_encode($data)
-	    );
+// pocket authorization url
+$url  = 'https://getpocket.com/v3/oauth/authorize';
+$data = array(
+    'consumer_key' => $config['consumer_key'],
+    'code' => $request_token
+);
 
-	// fetch response body
-	$result  = $response->body;
+// making post request with the data
+$response = Unirest::post($url, array(
+    "Content-Type" => "application/json; charset=UTF-8",
+    "X-Accept" => "application/json"
+), json_encode($data));
 
-	// check if access token is present
-	if(isset($result->access_token)){
-        $config['access_token']=$result->access_token;
-        $_SESSION['username'] = $result->username;
-        $_SESSION['config'] = $config;
-        
-        // redirecting to api page
-        header("location:api.php");
-	} else{
-		echo "Something went wrong. :( ";
-	}
+// fetch response body
+$result = $response->body;
+
+// check if access token is present
+if (isset($result->access_token)) {
+    $config['access_token'] = $result->access_token;
+    $_SESSION['username']   = $result->username;
+    $_SESSION['config']     = $config;
+    
+    // redirecting to api page
+    header("location:api.php");
+} else {
+    echo "Something went wrong. :( ";
+}
 
 
 ?>
